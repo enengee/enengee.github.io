5d4af994aa3e2bc59fb274ccec5aedef90aba3a0
nhannguyen95 <nhannguyenmath95@gmail.com>
2026-04-14 23:11:53 +0900

    Config for Github Pages

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://enengee.github.io/", // replace this with your deployed domain
+  author: "Nhan Nguyen",
+  profile: "https://enengee.dev/",
+  desc: "TBD-desc",
+  title: "TBD-title",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
