2ddd2a4836403f938aa268bd54d6d6442b67bd8a
nhannguyen95 <nhannguyenmath95@gmail.com>
2026-04-23 00:44:19 +0900

    Update email address

--- src/constants.ts
+++ src/constants.ts
@@ -37,7 +37,7 @@ export const SOCIALS: Social[] = [
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:nhanng@protonmail.com",
     linkTitle: `Send an email to ${SITE.author}`,
     icon: IconMail,
   },
